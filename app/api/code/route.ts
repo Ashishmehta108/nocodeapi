3cf96d614cb92d7b96f32e91e73bba99c01346d4
Ashish-cr <ashish@coderroots.com>
2026-03-04 21:44:52 +0530

    add code

--- app/api/code/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(request: NextRequest) {
-  return NextResponse.json({
-    message: "Hello world",
-  });
-}
-
-export default function handler(req: NextRequest) {}
